5f2408daca50b5db8bc07bf5e51a0c4a7895323c
Shreya Magar <shreyamagar0204@gmail.com>
2026-05-14 13:55:22 +0530

    Update API base URL to include '/api' path

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://her-backend-jofx.onrender.com",
+  baseURL: "https://her-backend-jofx.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
