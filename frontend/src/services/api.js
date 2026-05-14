46a3e105f6791bd31187ee1cf84623c02d63b4cf
Shreya Magar <shreyamagar0204@gmail.com>
2026-05-14 13:51:26 +0530

    Update API base URL to remove '/api' suffix

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://her-backend-jofx.onrender.com/api",
+  baseURL: "https://her-backend-jofx.onrender.com",
 });
 
 API.interceptors.request.use((config) => {
